0f967494b6299094ee424705cf669a858667e465
sladik-maksym <maxim.sl@darly.solutions>
2024-09-02 23:42:18 +0300

    refactor: change env gen

--- generate-env.ts
+++ generate-env.ts
@@ -14,12 +14,12 @@ if (!fs.existsSync(envFilePath)) {
     envFilePath,
     `export const environment = {
       firebase: {
-        apiKey: 'AIzaSyAmW_HumreBwhe-UIvhVuiUFtFYLC_7zWY',
-        authDomain: 'auth-angular-d85b0.firebaseapp.com',
-        projectId: 'auth-angular-d85b0',
-        storageBucket: 'auth-angular-d85b0.appspot.com',
-        messagingSenderId: '460788585618',
-        appId: '1:460788585618:web:6acc26cdc29ad0f18e024b',
+        apiKey: '',
+        authDomain: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: '',
       },
     };`
   );
@@ -30,12 +30,12 @@ if (!fs.existsSync(envDevFilePath)) {
     envDevFilePath,
     `export const environment = {
       firebase: {
-        apiKey: 'AIzaSyAmW_HumreBwhe-UIvhVuiUFtFYLC_7zWY',
-        authDomain: 'auth-angular-d85b0.firebaseapp.com',
-        projectId: 'auth-angular-d85b0',
-        storageBucket: 'auth-angular-d85b0.appspot.com',
-        messagingSenderId: '460788585618',
-        appId: '1:460788585618:web:6acc26cdc29ad0f18e024b',
+        apiKey: '',
+        authDomain: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: '',
       },
     };`
   );
